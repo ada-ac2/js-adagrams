fe828b32818df028a8a47a7334af05e44c50cf91
kelly <kelly.tate.dev@gmail.com>
2023-01-17 16:13:06 -0800

    Wave 3 Tests Passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -90,8 +90,55 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 };
 
-const scoreWord = (word) => {
+export const scoreWord = (word) => {
   // Implement this method for wave 3
+  if (word === '') {
+    return 0;
+  }
+  
+  word = word.toUpperCase();
+
+  let points = 0;
+
+  const scoreChart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+  }
+
+  for (const letter of word) {
+    points += scoreChart[letter];
+  }
+
+  if (word.length >= 7 && word.length <= 10) {
+    points += 8;
+  }
+
+  return points;
+
 };
 
 const highestScoreFrom = (words) => {
