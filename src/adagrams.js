d4f79a368535c88d6a8142f66bbaa4d1619fc6cb
Lisa Utsett <lisabethutsett@gmail.com>
2023-01-17 22:16:15 -0500

    passed wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,39 +1,32 @@
 "use strict";
-
 const letterPool = {
-  'A': 9, 
-  'B': 2, 
-  'C': 2, 
-  'D': 4, 
-  'E': 12, 
-  'F': 2, 
-  'G': 3, 
-  'H': 2, 
-  'I': 9, 
-  'J': 1, 
-  'K': 1, 
-  'L': 4, 
-  'M': 2, 
-  'N': 6, 
-  'O': 8, 
-  'P': 2, 
-  'Q': 1, 
-  'R': 6, 
-  'S': 4, 
-  'T': 6, 
-  'U': 4, 
-  'V': 2, 
-  'W': 2, 
-  'X': 1, 
-  'Y': 2, 
-  'Z': 1
-};
+    A: 9, B: 2, C: 2, D: 4, 
+    E: 12, F: 2, G: 3, H: 2, 
+    I: 9, J: 1, K: 1, L: 4, 
+    M: 2, N: 6, O: 8, P: 2, 
+    Q: 1, R: 6, S: 4, T: 6, 
+    U: 4, V: 2, W: 2, X: 1, 
+    Y: 2, Z: 1
+  };
+  
 
 export const drawLetters = () => {
-  // returns an array of ten strings
-  // each string should be 1 letter
-  // randomly drawn letters Math.random()
-  // 
+    let bagOfLetters = [];
+    const drawnLetters = [];
+    
+    for (let letter in letterPool) {
+      for (let i = 0; i < letterPool[letter]; ++i) {
+        bagOfLetters.push(letter);
+      }
+    }
+
+    for (let i = 0; i < 10; ++i) {
+      let randomIndex = Math.floor((Math.random() * bagOfLetters.length));
+      let randomLetter = bagOfLetters[randomIndex]; 
+      drawnLetters.push(randomLetter);
+      bagOfLetters.splice(randomIndex, 1); 
+    }
+    return drawnLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
