f21a7da993fc4d81bb9f9d3d4a50e1396f4f3540
Xuan Hien Pham <phamxuanhien02@gmail.com>
2023-01-20 18:56:32 -0500

    passed wave 2 and 3. And added a test for scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -29,6 +29,35 @@ const LETTER_POOL = {
 
 const LEN = 26;
 
+const scoreChart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   let letterFrequency = {};
   let aHandOfLetters = [];
@@ -51,11 +80,34 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let found = 0;
+  for (let char of input) {
+    for (let i in lettersInHand) {
+      if (char === lettersInHand[i]) {
+        lettersInHand[i] = "";
+        found++;
+        break;
+      }
+    }
+  }
+  if (found === input.length) {
+    return true;
+  }
+  return false;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  // if (word.length === 0) {
+  //   return 0;
+  // }
+  let totalPoints = 0;
+  for (let char of word) {
+    totalPoints += scoreChart[char.toUpperCase()];
+  }
+  if (word.length >= 7) {
+    totalPoints += 8;
+  }
+  return totalPoints;
 };
 
 export const highestScoreFrom = (words) => {
