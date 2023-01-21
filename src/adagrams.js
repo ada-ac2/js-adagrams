1d378042e8d496f72f270c18af39cea73b609e8b
diana <arreoladiana11@gmail.com>
2023-01-20 19:08:00 -0800

    wave3: complete scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const letterPool = {
   Z: 1,
 };
 
+const letterValues = {
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 2, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10
+}
+
 export const drawLetters = () => {
   let letterPoolCopy = JSON.parse(JSON.stringify(letterPool));
   const letters = [];
@@ -45,7 +74,7 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   let letterBankCopy = JSON.parse(JSON.stringify(lettersInHand));
-  
+
   for (let i = 0; i < input.length; i++) {
     if (letterBankCopy.includes(input[i])) {
       delete letterBankCopy[i];
@@ -57,7 +86,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0
+  for (const letter of word) {
+    score += letterValues[letter.toUpperCase()]
+  }
+
+  if (word.length > 6 && word.length < 11){
+    score += 8
+  }
+  return score
 };
 
 export const highestScoreFrom = (words) => {
