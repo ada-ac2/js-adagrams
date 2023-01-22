e4f9f234a9e0adcbdd4a0fc3345afe9c79d8be72
larissa <larza.ault@gmail.com>
2023-01-21 21:23:13 -0800

    Finished solution for drawLetters and passed all wave 1 tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,37 @@
+import { Alphabet } from "./alphabet.js";
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  // initialize player hand
+  let hand = [];
+  // initialize alphabet object
+  let alphabet = new Alphabet();
+  // draw 10 letters
+  for (let i = 0; i < 10; ++i) {
+    // get array of letters (keys) from alphabet object
+    const keys = Object.keys(alphabet.object);
+    // randomly pick one letter (key)
+    const letter = keys[Math.floor(Math.random() * keys.length)];
+    // add letter to hand
+    hand.push(letter);
+    // letter value in alphabet object
+    alphabet.decreaseValue(letter);
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  // convert input string to array
+  const inputArr = [...input];
+  // make mutable copy of letters in hand
+  let handArr = [...lettersInHand];
+  // check each letter in input
+  inputArr.forEach((letter) => {
+    if (lettersInHand.includes(letter)) {
+    }
+  });
+
+  // return true if every letter in input in lettersInHand
+  return inputArr.every((letter) => lettersInHand.includes(letter));
 };
 
 export const scoreWord = (word) => {
