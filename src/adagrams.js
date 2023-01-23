6895f068a8b00e7223cc4a9ae64183a1b2f86bda
Yufei Bao <ybao1005@gmail.com>
2023-01-22 16:49:24 -0800

    Project completed

--- src/adagrams.js
+++ src/adagrams.js
@@ -99,3 +99,28 @@ export const scoreWord = (word) => {
   return score 
 }
 
+export const highestScoreFrom = (words) => {
+  let highestScoreWord = {
+    score: 0,
+    word: ""
+  } 
+
+  for(const word of words){
+    const currScore = scoreWord(word); 
+    if(currScore > highestScoreWord["score"]){
+      highestScoreWord["word"] = word; 
+      highestScoreWord["score"] = currScore; 
+    }else if(currScore === highestScoreWord["score"]){
+      if(highestScoreWord["word"].length === 10){
+        continue; 
+      } else if(word.length === 10){
+        highestScoreWord["word"] = word; 
+        continue; 
+      }else if(word.length < highestScoreWord["word"].length){
+        highestScoreWord["word"] = word;  
+      }
+    }
+  }
+  
+  return highestScoreWord
+};
