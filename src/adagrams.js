0bf8384c669859c1eb810f4f5417af3b43da9f83
larissa <larza.ault@gmail.com>
2023-01-22 16:10:55 -0800

    Completed scoreWord function and passed all wave 3 unit tests (4/4)

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,31 +1,30 @@
-import { Alphabet } from "./alphabet.js";
-import { ScoreChart } from "./scoreChart.js";
+import { Alphabet } from "./modules/Alphabet.js";
+import { Score } from "./modules/Score.js";
 
 export const drawLetters = () => {
   // initialize player hand
   let hand = [];
-  // initialize alphabet object
+  // initialize alphabet
   let alphabet = new Alphabet();
   // draw 10 letters
   for (let i = 0; i < 10; ++i) {
     // get array of letters (keys) from alphabet object
-    const keys = Object.keys(alphabet.object);
+    const keys = Object.keys(alphabet.letterCounts);
     // randomly pick one letter (key)
     const letter = keys[Math.floor(Math.random() * keys.length)];
     // add letter to hand
     hand.push(letter);
     // letter value in alphabet object
-    alphabet.decreaseValue(letter);
+    alphabet.decreaseLetterCount(letter);
   }
   return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  /////////// HASH TABLE IMPLEMENTATION - OPPORTUNITY TO REFACTOR /////////////////
   // convert input string to array
   const inputArr = [...input.toUpperCase()];
   // make mutable copy of letters in hand
-  let handArr = [...lettersInHand.toUpperCase()];
+  let handArr = [...lettersInHand];
   for (let i = 0; i < inputArr.length; i++) {
     // collect letter
     const letter = inputArr[i];
@@ -43,22 +42,12 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // initialize word score
-  let score = 0;
-  // initialize score chart
-  const scoreChart = new ScoreChart();
-  // convert word string to array
-  const wordArr = [...word.toUpperCase()];
-  // check each letter in word
-  wordArr.forEach((letter, i) => {
-    score += scoreChart.object[letter];
-    if (i === 7) {
-      score += 8;
-    }
-  });
-  // for (let letter of wordArr) {
-  // look up letter in score chart and add score
-  return score;
+  // check for word input
+  if (typeof word === "undefined") {
+    return 0;
+  } else {
+    return new Score().calculateWordScore(word);
+  }
 };
 
 export const highestScoreFrom = (words) => {
