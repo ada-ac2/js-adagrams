49a209be9985bcd790a6b45060f70298d85ef6db
Jennifer Dai <jendai173@gmail.com>
2023-02-01 12:01:13 -0800

    fixed the infinite loop due to the global letter pool. move it to draw letter function.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,32 +1,32 @@
 //letter pool
-  const LETTER_POOL = {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1,
-  };
+  // const LETTER_POOL = {
+  //   A: 9,
+  //   B: 2,
+  //   C: 2,
+  //   D: 4,
+  //   E: 12,
+  //   F: 2,
+  //   G: 3,
+  //   H: 2,
+  //   I: 9,
+  //   J: 1,
+  //   K: 1,
+  //   L: 4,
+  //   M: 2,
+  //   N: 6,
+  //   O: 8,
+  //   P: 2,
+  //   Q: 1,
+  //   R: 6,
+  //   S: 4,
+  //   T: 6,
+  //   U: 4,
+  //   V: 2,
+  //   W: 2,
+  //   X: 1,
+  //   Y: 2,
+  //   Z: 1,
+  // };
 
   const LETTER_VALUE = {
     A: 1, 
@@ -59,16 +59,45 @@
 
 
 export const drawLetters = () => {
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
   // find the random letters
   let letters = [];
-  for(let i = 0; letters.length < 10; ++i){
-    let letter = String.fromCharCode(Math.floor(Math.random()*26)+ 65)
+  while (letters.length < 10){
+    let letter = String.fromCharCode(Math.floor(Math.random()*26)+ 65);
+    
     //check whether letter is available in the letter pool
     if(LETTER_POOL[letter] > 0){
-      letters.push(letter);
       LETTER_POOL[letter] -= 1;
-    } 
-  }
+      letters.push(letter);
+    }; 
+  };
   return letters;
   
 };
